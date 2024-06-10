453cc4dbc43697ffaaff110cb2b76acccedf2790
MikkelChristensen <43296294+MikkelChristensenGit@users.noreply.github.com>
2024-06-10 12:21:33 +0200

    Create birds_mobile.py

---
+++ birds_mobile.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd
+import os
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, GlobalAveragePooling2D, Dropout, Flatten
+from tensorflow.keras.applications import VGG16
+import matplotlib.pyplot as plt
+import seaborn as sns
+print(os.getcwd())
+os.chdir(r'C:\Users\Bregning\BirdClassifier')
+#%%
+
+train_dir = 'train'
+val_dir = 'test'
+test_dir = 'test'
+
+batch_size = 32
+img_size = 224
+
+train_datagen = ImageDataGenerator(rescale=1/255.,
+                             zoom_range=0.0,
+                            width_shift_range=0.0,height_shift_range=0.0
+                             )
+
+val_datagen = ImageDataGenerator(rescale=1/255.)
+
+test_datagen = ImageDataGenerator(rescale=1/255.)
+
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                target_size=(img_size, img_size),
+                                                batch_size=batch_size,
+                                                shuffle=True,
+                                                class_mode='categorical')
+
+validation_generator = val_datagen.flow_from_directory(val_dir,
+                                                    target_size=(img_size, img_size),
+                                                    batch_size=batch_size,
+                                                    shuffle=False,
+                                                    class_mode='categorical')
+
+test_generator = test_datagen.flow_from_directory(test_dir,
+                                                    target_size=(img_size, img_size),
+                                                    batch_size=batch_size,
+                                                    shuffle=False,
+                                                    class_mode='categorical')
+
+#%%
+
+labels = [k for k in train_generator.class_indices]
+sample_generate = train_generator.__next__()
+
+images = sample_generate[0]
+titles = sample_generate[1]
+plt.figure(figsize = (20 , 20))
+
+for i in range(15):
+    plt.subplot(5 , 5, i+1)
+    plt.subplots_adjust(hspace = 0.3 , wspace = 0.3)
+    plt.imshow(images[i])
+    plt.title(f'Class: {labels[np.argmax(titles[i],axis=0)]}')
+    plt.axis("off")
+
+#%%
+from tensorflow.keras.applications import MobileNetV2
+base_model = MobileNetV2(include_top = False,
+                       weights = 'imagenet',
+                       input_shape = (img_size,img_size,3))
+
+num_layers = len(base_model.layers)
+print(num_layers)
+#%%
+for layer in base_model.layers[:num_layers//2]:
+    layer.trainable = False
+
+model = Sequential()
+model.add(base_model)
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5)) # to prevent overfitting
+model.add(Dense(525, activation='softmax'))
+
+#%%
+
+from tensorflow.keras.optimizers import Adam
+
+model.compile(optimizer=Adam(learning_rate=0.0001),
+             loss='categorical_crossentropy',
+             metrics=['accuracy'])
+#%%
+
+history = model.fit(train_generator,
+                    epochs=10,
+                    validation_data=validation_generator,
+                   )
